2a93dd90e01565a302adf94385c1021f1776ba1e
PhilippFu <Philipp.Fuxen@bfft.de>
2019-07-12 12:11:46 +0200

    add docs to generate sphinx docu
    better docu

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = '11.07.2019'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
